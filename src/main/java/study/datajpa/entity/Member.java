7f420cfad93a1f6a21aa1f4d0029c044e6a6f01e
ShinMinOh <luciano97@naver.com>
2023-10-01 16:49:54 +0900

    Made MemberJpaRepository(No use of Spring Data JPA)

--- src/main/java/study/datajpa/entity/Member.java
+++ src/main/java/study/datajpa/entity/Member.java
@@ -1,20 +1,43 @@
 package study.datajpa.entity;
 
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
-import lombok.Getter;
-import lombok.Setter;
+import jakarta.persistence.*;
+import lombok.*;
 
 @Entity
-@Getter @Setter
-
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)   // 이 어노테이션이 있으면 기본 생성자 안써줘도 됨.
+@ToString(of = {"id", "username", "age"})           // Team 같은 연관관계 필드는 ToString에 포함하지 않기. 타고 가서 다 출력될수 있음.
 public class Member {
 
     @Id @GeneratedValue
+    @Column(name = "member_id")
     private Long id;
-
     private String username;
+    private int age;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "team_id")
+    private Team team;
+
+
+
+    public Member(String username) {
+        this.username = username;
+    }
+
+    public Member(String username, int age, Team team) {
+        this.username = username;
+        this.age = age;
+        if(team != null){
+            changeTeam(team);
+        }
+    }
+
+    // 연관관계 편의 메서드. 양방향 연관관계 처리.
+    public void changeTeam(Team team){ //Team은 연관관계를 맺고 있는 객체이기 때문에 team뿐만 아니라 member도 바꿔줘야함.
+        this.team = team;
+        team.getMembers().add(this);
+    }
 
 }
--- src/main/java/study/datajpa/repository/MemberJpaRepository.java
+++ src/main/java/study/datajpa/repository/MemberJpaRepository.java
@@ -1,10 +1,20 @@
-package study.datajpa.entity.repository;
+package study.datajpa.repository;
 
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
+import java.util.List;
+import java.util.Optional;
 import org.springframework.stereotype.Repository;
 import study.datajpa.entity.Member;
 
+/**
+ * Spring Data Jpa 사용 안하는 레포지토리
+ * /
+
+/**
+ * CRUD 중 하나인 Update는 필요가 없다
+ * ->em을 통해서 조회 해온 후, entity를 직접 수정하고 트랜잭션을 commit하면 자동으로 변경된것을 감지하여 DB에 update 쿼리를 날려준다.
+ * */
 @Repository
 public class MemberJpaRepository {
 
@@ -16,7 +26,30 @@ public class MemberJpaRepository {
         return member;
     }
 
-    public Member find(Long id){            // 조회
+    public void delete(Member member){
+        em.remove(member);
+    }
+
+    // 전체조회
+    public List<Member> findAll(){
+        return em.createQuery("select m from Member m", Member.class)
+                    .getResultList();
+    }
+
+    // id로 조회
+    public Optional<Member> findById(Long id){
+        Member member = em.find(Member.class, id);
+        return Optional.ofNullable(member);
+    }
+
+    // 카운트
+    public long count(){
+        return em.createQuery("select count(m) from Member m", Long.class)
+                 .getSingleResult(); //count 하면 Long타입으로 반환됨.
+    }
+
+    // 단건조회
+    public Member find(Long id){
         return em.find(Member.class, id);
     }
 
