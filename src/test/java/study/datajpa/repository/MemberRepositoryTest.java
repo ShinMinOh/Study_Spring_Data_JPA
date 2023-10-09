5eee537f4f102d26158a2472b6c0f9322e0709a5
ShinMinOh <luciano97@naver.com>
2023-10-09 15:58:23 +0900

    queryHint 학습

--- src/test/java/study/datajpa/repository/MemberRepositoryTest.java
+++ src/test/java/study/datajpa/repository/MemberRepositoryTest.java
@@ -288,6 +288,20 @@ class MemberRepositoryTest {
             System.out.println("member.teamClass = "+ member.getTeam().getClass());
             System.out.println("member.team = "+ member.getTeam().getName());
         }
+    }
+
+    @Test
+    public void queryHint(){
+        //given
+        Member member1 = new Member("member1", 10);
+        memberRepository.save(member1);
+        em.flush();
+        em.clear();
+
+        //when
+        Member findMember = memberRepository.findById(member1.getId()).get();
+        findMember.setUsername("member2");
+
 
     }
 }
