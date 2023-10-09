1eb20b99fa98af109028b305c346df01ab65a02c
ShinMinOh <luciano97@naver.com>
2023-10-09 16:01:19 +0900

    queryHint 학습

--- src/main/java/study/datajpa/repository/MemberRepository.java
+++ src/main/java/study/datajpa/repository/MemberRepository.java
@@ -1,5 +1,6 @@
 package study.datajpa.repository;
 
+import jakarta.persistence.QueryHint;
 import java.util.Collection;
 import java.util.List;
 import java.util.Optional;
@@ -9,6 +10,7 @@ import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.jpa.repository.QueryHints;
 import org.springframework.data.repository.query.Param;
 import study.datajpa.dto.MemberDto;
 import study.datajpa.entity.Member;
@@ -85,5 +87,10 @@ public interface    MemberRepository extends JpaRepository<Member, Long> {
 
     @EntityGraph(attributePaths = {"team"})     //team 엔티티까지 같이 가져오기
     List<Member> findEntityGraphByUsername(@Param("username") String username);
+
+
+    // 11. 쿼리 힌트(변경감지(update) 적용X)
+    @QueryHints(value = @QueryHint(name = "org.hibernate.readOnly", value = "true"))
+    Member findReadOnlyByUsername(String username);
 }
 
